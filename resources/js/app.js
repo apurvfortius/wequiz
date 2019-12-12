b99727d7a4e243ac51d1a752a6833b27dc0e4683
apurvfortius <apurv.bhavasar@fortiustechsolutions.com>
2019-12-12 16:31:20 +0530

    Add existing file

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+let Myheader = require('./components/Header.vue').default;
+let MyFooter = require('./components/Footer.vue');
+
+const app = new Vue({
+    el : '#app',
+    components : {Myheader, MyFooter}
+});
